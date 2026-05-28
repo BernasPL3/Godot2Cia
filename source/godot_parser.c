3e5f1a053b4ca9e06e64924cbc77d168f7a9b0dc
BernasPL3 <bernardogamesguidi@gmail.com>
2026-05-28 18:22:39 -0300

    Create godot_parser.c

---
+++ source/godot_parser.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void parseGodotProject(const char* projectFile) {
+
+    printf("Lendo projeto Godot: %s\n", projectFile);
+
+    // Ler project.godot
+    // Ler cenas .tscn
+    // Ler assets
+
+    printf("Projeto carregado!\n");
+}
