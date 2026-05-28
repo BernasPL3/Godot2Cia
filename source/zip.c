17c8d02b77e0fb17c5e237e5aa78dc8fb5469028
BernasPL3 <bernardogamesguidi@gmail.com>
2026-05-28 18:20:35 -0300

    Create zip.c

---
+++ source/zip.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void compressProject(const char* projectName) {
+
+    printf("Compactando projeto: %s\n", projectName);
+
+    // Aqui vai sistema ZIP futuramente
+
+    printf("ZIP criado!\n");
+}
