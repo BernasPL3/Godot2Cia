198604d31cc140ff678fdbffb64a341dc796cb95
BernasPL3 <bernardogamesguidi@gmail.com>
2026-05-28 18:23:07 -0300

    Create ui.c

---
+++ source/ui.c
@@ -0,0 +1,14 @@
+#include <3ds.h>
+#include <stdio.h>
+
+void drawMenu() {
+
+    printf("========================\n");
+    printf("       Godot2CIA\n");
+    printf("========================\n");
+
+    printf("[A] Abrir Projeto\n");
+    printf("[B] Compactar\n");
+    printf("[X] Criar CIA\n");
+    printf("[Y] Configuracoes\n");
+}
